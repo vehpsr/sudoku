7167baf87471e53ec96cd318d728e50617cc1e5b
SHusak <shusak@cvent.com>
2016-10-18 22:22:07 +0300

    initial commit

---
+++ src/main/java/com/gans/sudoku/App.java
@@ -0,0 +1,12 @@
+package com.gans.sudoku;
+
+public class App {
+
+    private static final Random RAND = new Random();
+
+    public static void main(String[] args) {
+        new App();
+    }
+
+
+}
